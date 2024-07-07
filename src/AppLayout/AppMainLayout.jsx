eba0e05702e64ad281fb32cb668409674ba2ec01
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-07 11:46:48 +0530

    tw-btn

--- src/AppLayout/AppMainLayout.jsx
+++ src/AppLayout/AppMainLayout.jsx
@@ -4,7 +4,7 @@ import Header from "../Header/Header";
 export default function AppLayout(){
     return(
         <>
-        <header className="border-solid border border-sky-500 py-7 flex gap-5 px-2 w-full">
+        <header className="border-solid border border-sky-500 py-7 flex gap-5 px-2 w-full justify-center ">
            <Header/>
            
         </header>
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header(){
     return(
         <>
-        <div className="flex w-full border-solid border border-blue-600 justify-center ">
+        <div className="flex w-full justify-center items-center">
         <div className="mr-auto justify-start">
             <h1>LOGO</h1>
         </div>
-        <div className="flex flex-wrap gap-5 border-solid border border-green-500 ">
+        <div className="flex flex-wrap gap-5 border-solid border border-green-500 justify-center items-center">
             <span>About</span>
             <span>Services</span>
             <span>Cases</span>
@@ -14,8 +14,8 @@ export default function Header(){
 
 
         </div>
-        <div className=" border-solid border border-red-600 ml-auto justify-end">
-            <button>login</button>
+        <div className=" border-solid ml-auto justify-end">
+            <button className="px-12 py-3 text-white rounded-md bg-gradient-to-r from-blue-600  to-cyan-400 hover:from-pink-500 hover:to-yellow-500"  >login</button>
         </div>
         </div>
         
