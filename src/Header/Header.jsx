771ffce792a3a2e2fe61bf04deeb364ce87513a1
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-07 19:50:58 +0530

    responsiveness

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header(){
     return(
         <>
-        <div className="flex w-full justify-center items-center">
-        <div className="mr-auto justify-start">
+        <div className="flex w-full justify-center items-center gap-10">
+        <div className=" justify-start">
             <h1>LOGO</h1>
         </div>
         <div className="flex flex-wrap gap-5 border-solid border border-green-500 justify-center items-center">
@@ -14,8 +14,8 @@ export default function Header(){
 
 
         </div>
-        <div className=" border-solid ml-auto justify-end flex gap-5">
-            <button className="">Login</button>
+        <div className=" border-solid font-semibold ml-auto justify-end flex gap-5">
+            <button className=" text-lg">Log in</button>
             <button className="px-6 py-2 text-black rounded-lg border border-slate-950 font-bold" >Sign up free</button>
         </div>
         </div>
