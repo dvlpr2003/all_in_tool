7e5bfb0892132f43ac0c2a176b5365ac1fa74a8e
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-08 11:44:53 +0530

    header-arrow

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react"
-
+import { MdKeyboardArrowDown } from "react-icons/md";
 export default function Header(){
     const [enter,setEnter]=useState(false)
     return(
         <>
-        <div className="flex w-full justify-center items-center gap-10  ">
+        <div className="flex w-full justify-center items-center gap-10 max-w-screen-xl ">
 
 
         <div className=" justify-start">
@@ -15,7 +15,7 @@ export default function Header(){
 
         <div className="flex flex-wrap font-semibold justify-center items-center cursor-pointer">
             
-            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-2 text-base`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)} ><span>About</span></div>
+            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-2 text-base flex items-center justify-center `} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)} ><span>About</span> <MdKeyboardArrowDown className="c-menu-arrow"/></div>
             <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-2 text-base`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)}><span>Services</span></div>
             <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-2 text-base`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)}><span>Cases</span></div>
             <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-2 text-base`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)}><span>Blog</span></div>
