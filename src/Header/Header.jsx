559d0608b7f9ac3dc5052d0fc7ece74cc58cec6e
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-08 07:39:17 +0530

    header-comp

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,22 +1,25 @@
+import { useState } from "react"
+
 export default function Header(){
+    const [enter,setEnter]=useState(false)
     return(
         <>
         <div className="flex w-full justify-center items-center gap-10">
         <div className=" justify-start">
             <h1>LOGO</h1>
         </div>
-        <div className="flex flex-wrap gap-8 font-semibold border-solid border border-green-500 justify-center items-center">
-            <span>About</span>
-            <span>Services</span>
-            <span>Cases</span>
-            <span>Blog</span>
-            <span>Contact</span>
-
+        <div className="flex flex-wrap font-semibold justify-center items-center cursor-pointer">
+            
+            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-2 text-lg`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)} ><span>About</span></div>
+            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-2 text-lg`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)}><span>Services</span></div>
+            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-2 text-lg`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)}><span>Cases</span></div>
+            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-2 text-lg`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)}><span>Blog</span></div>
+            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-2 text-lg`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)}><span>Contact</span></div>
 
         </div>
         <div className=" border-solid font-semibold ml-auto justify-end flex gap-5">
             <button className=" px-5 text-lg rounded-lg hover:bg-slate-100">Log in</button>
-            <button className="px-6 py-2 text-black rounded-lg border border-slate-950 font-bold  hover:bg-red-500 hover:border-red-500 hover:text-white" >Sign up free</button>
+            <button className="px-6 py-2 text-lg text-black rounded-lg border border-slate-950 font-bold  hover:bg-red-500 hover:border-red-500 hover:text-white" >Sign up free</button>
         </div>
         </div>
         
