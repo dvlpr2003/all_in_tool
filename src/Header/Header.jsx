1cd548e321f22c71048c0466e2a71ada1eb1c279
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-08 07:51:24 +0530

    header-comp

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -5,9 +5,14 @@ export default function Header(){
     return(
         <>
         <div className="flex w-full justify-center items-center gap-10">
+
+
         <div className=" justify-start">
+
             <h1>LOGO</h1>
+
         </div>
+        
         <div className="flex flex-wrap font-semibold justify-center items-center cursor-pointer">
             
             <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-2 text-lg`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)} ><span>About</span></div>
@@ -17,10 +22,15 @@ export default function Header(){
             <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-2 text-lg`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)}><span>Contact</span></div>
 
         </div>
+
         <div className=" border-solid font-semibold ml-auto justify-end flex gap-5">
+
             <button className=" px-5 text-lg rounded-lg hover:bg-slate-100">Log in</button>
             <button className="px-6 py-2 text-lg text-black rounded-lg border border-slate-950 font-bold  hover:bg-red-500 hover:border-red-500 hover:text-white" >Sign up free</button>
+
         </div>
+
+
         </div>
         
         </>
