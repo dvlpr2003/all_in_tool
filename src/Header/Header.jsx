34eae856be9f0c035917b0855bf137921576e06c
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-09 12:58:52 +0530

    header-response

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,13 +1,14 @@
 import { useState,useEffect } from "react"
 import { MdKeyboardArrowDown } from "react-icons/md";
+import IconMenu from "./MenuIcon";
 export default function Header(){
     const [enter,setEnter]=useState(false)
     const [About,setAbout]=useState(false)
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= 1000);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
 // screen innerWidth listener useEffect function
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 1000);
+      setIsMobile(window.innerWidth <= 1024);
     };
 
     window.addEventListener('resize', handleResize);
@@ -30,28 +31,31 @@ export default function Header(){
 
         </div>
 
-        <div className="flex flex-wrap font-semibold justify-center items-center cursor-pointer">
+        <div className="flex flex-wrap font-semibold justify-center items-center cursor-pointer sm:hidden lg:flex min-[55px]:hidden">
             
             <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-3 text-base flex items-center justify-center relative `} onMouseLeave={!isMobile?()=>{setEnter(false); setAbout(false)}:null} onMouseEnter={!isMobile?()=>{setEnter(true); setAbout(true)}:null} onClick={isMobile?()=>{setEnter((e)=>!e); setAbout((e)=>!e)}:()=>{setEnter((e)=>!e); setAbout((e)=>!e)}} >
                 <div className="flex" >
                 <span >Tools</span>
-             <MdKeyboardArrowDown className="c-menu-arrow"/> 
+                <MdKeyboardArrowDown className="c-menu-arrow"/> 
+                </div>
+            <ProductDt About={About} setAbout={setAbout} enter={enter} setEnter={setEnter} isMobile={isMobile}/>
             </div>
-        <ProductDt About={About} setAbout={setAbout} enter={enter} setEnter={setEnter} isMobile={isMobile}/>
 
-            </div>
-            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-3 text-base`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)}><span>Services</span></div>
-            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-3 text-base`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)}><span>Cases</span></div>
-            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-3 text-base`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)}><span>Blog</span></div>
-            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-3 text-base`} onPointerLeave={()=>setEnter(false)} onPointerEnter={()=>setEnter(true)}><span>Contact</span></div>
+            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-3 text-base`} onMouseLeave={()=>setEnter(false)} onMouseEnter={()=>setEnter(true)}><span>Services</span></div>
+            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-3 text-base`} onMouseLeave={()=>setEnter(false)} onMouseEnter={()=>setEnter(true)}><span>Cases</span></div>
+            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-3 text-base`} onMouseLeave={()=>setEnter(false)} onMouseEnter={()=>setEnter(true)}><span>Blog</span></div>
+            <div className={`text-slate-950 hover:text-black ${enter?'text-zinc-500':""} px-5 py-3 text-base`} onMouseLeave={()=>setEnter(false)} onMouseEnter={()=>setEnter(true)}><span>Contact</span></div>
 
         </div>
 
-        <div className=" border-solid font-semibold ml-auto justify-end flex gap-5">
-
+        <div className=" border-solid font-semibold ml-auto justify-end ">
+            <div className="sm:block lg:hidden">
+            <IconMenu />
+            </div>
+            <div className="flex gap-5 sm:hidden min-[55px]:hidden lg:flex">
             <button className=" px-5 text-lg rounded-lg hover:bg-slate-100">Log in</button>
             <button className="px-6 py-2 text-lg text-black rounded-lg border border-slate-950 font-bold  hover:bg-red-500 hover:border-red-500 hover:text-white" >Sign up free</button>
-
+            </div>
         </div>
 
 
---
+++ src/Header/MenuIcon.jsx
@@ -0,0 +1,9 @@
+import { HiOutlineMenuAlt1 } from "react-icons/hi";
+function IconMenu(){
+    return(
+        <>
+        <HiOutlineMenuAlt1 className="menu-icon"/>
+        </>
+    )
+}
+export default IconMenu;
