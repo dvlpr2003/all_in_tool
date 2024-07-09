9c2b6d416e3c54227b471fb3e52f1456fa57c292
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-09 13:37:45 +0530

    header-response

--- src/Header/MenuIcon.jsx
+++ src/Header/MenuIcon.jsx
@@ -2,7 +2,7 @@ import { HiOutlineMenuAlt1 } from "react-icons/hi";
 function IconMenu(){
     return(
         <div className="flex justify-center items-center gap-5">
-        <span>Menu</span>
+        <span className="text-2xl">Menu</span>
         <HiOutlineMenuAlt1 className="menu-icon"/>
         </div>
     )
