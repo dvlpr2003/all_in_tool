9e2d92e052c55f8935f3d0afd989ae33328363c7
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-12 11:56:47 +0530

    header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -55,7 +55,7 @@ export default function Header(){
             </div>
             <div className="flex gap-5 sm:hidden min-[55px]:hidden lg:flex">
             <button className=" px-5 text-lg rounded-lg hover:bg-slate-100 border-0">Log in</button>
-            <button className="px-6 py-2 text-lg text-black rounded-lg border border-slate-950 font-bold  hover:bg-indigo-600 hover:border-indigo-600 hover:text-white" >Sign up free</button>
+            <button className="px-6 py-2 text-lg text-white rounded-lg border border-indigo-600 bg-indigo-600 font-bold  hover:bg-indigo-500 hover:border-indigo-500 hover:text-white" >Sign up free</button>
             </div>
         </div>
 
