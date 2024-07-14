4ca8f23c4969c820bacab51e0333a58194042916
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-14 11:06:13 +0530

    logo and icons

--- src/Header/Submenu.jsx
+++ src/Header/Submenu.jsx
@@ -62,6 +62,63 @@ export function ProductDt({About,enter,setAbout,setEnter,isMobile}){
                         </ul>
 
                     </div>
+                    <div>
+                    <div className="border border-white w-60  bg-white py-5 pb-6 rounded-lg hover:shadow-sm">
+                        <span className="ml-6">Convert to PDF</span>
+                        <ul className="flex flex-col gap-5 justify-center items-center mt-5">
+                            <li className="w-48 py-2 hover:border-blue-100 hover:bg-blue-100 hover:rounded-sm hover:shadow-md">
+                                <a href="#" className="flex h-9 items-center" >
+                                    <div className="flex justify-center items-center gap-6">
+                                    <img src="img/convert pdf/jpeg to pdf.png" alt="" className="max-w-full h-10"/>
+                                    <span>JPG to PDF</span>
+                                    </div>
+                                </a>
+                            </li>
+                            <li className=" w-48 py-2 hover:border-violet-100 hover:bg-violet-100 hover:rounded-sm hover:shadow-md">
+                                <a href="#" className="flex h-9 items-center">
+                                <div className="flex justify-center items-center gap-6">
+                                   <img src="img/convert pdf/jpeg to pdf.png" alt="" className="max-w-full h-10"/>
+                                    <span>PNG to PDF</span>
+                                </div>
+                                </a>
+                            </li>
+
+                            <li className=" w-48 py-2 hover:border-violet-100 hover:bg-violet-100 hover:rounded-sm hover:shadow-md">
+                            <a href="#" className="flex h-9 items-center">
+                                <div className="flex justify-center items-center gap-6">
+                                   <img src="img/convert pdf/html to pdf.png" alt="" className="max-w-full h-10"/>
+                                    <span>HTML to PDF</span>
+                                </div>
+                                </a>
+                            </li>
+                            <li className="w-48 py-2 hover:border-zinc-100 hover:bg-zinc-100 hover:rounded-sm hover:shadow-md">
+                                <a href="#" className="flex h-9 items-center">
+                                <div className="flex justify-center items-center gap-6">
+                                   <img src="img/convert pdf/word to pdf.png" alt="" className="max-w-full h-10"/>
+                                    <span>WORD to PDF</span>
+                                </div>
+                                </a>
+                            </li>
+                            <li className="w-48 py-2 hover:border-zinc-100 hover:bg-zinc-100 hover:rounded-sm hover:shadow-md">
+                                <a href="#" className="flex h-9 items-center">
+                                <div className="flex justify-center items-center gap-6">
+                                   <img src="img/convert pdf/excel to pdf.png" alt="" className="max-w-full h-10"/>
+                                    <span>EXCEL to PDF</span>
+                                </div>
+                                </a>
+                            </li>
+                            <li className="w-48 py-2 hover:border-zinc-100 hover:bg-zinc-100 hover:rounded-sm hover:shadow-md">
+                                <a href="#" className="flex h-9 items-center">
+                                <div className="flex justify-center items-center gap-6">
+                                   <img src="img/convert pdf/ppt to pdf.png" alt="" className="max-w-full h-10"/>
+                                    <span>PPT to PDF</span>
+                                </div>
+                                </a>
+                            </li>
+                        </ul>
+
+                    </div>
+                    </div>          
     
                     <div>
                         <div className="border border-white w-60  bg-white py-5 pb-6 rounded-lg  hover:shadow-sm">
