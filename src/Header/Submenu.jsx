7bd4d8ed8292dfa729c16a59bf5d2fa2825b5be6
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-14 10:19:14 +0530

    logo and icons

--- src/Header/Submenu.jsx
+++ src/Header/Submenu.jsx
@@ -7,78 +7,62 @@ export function ProductDt({About,enter,setAbout,setEnter,isMobile}){
         <div className={` absolute border about-menu ${About?"flex":"hidden"} gap-5 text-black  shadow-md`}>
             <div className=" w-full mt-9 flex justify-center pb-7">
                 <div className="flex gap-7 ">
-                    <div className="border border-white w-60  bg-white py-5 pb-6 rounded-lg hover:shadow-sm">
-                        <span className=" ml-6">Convert to PDF</span>
+                <div className="border border-white w-60  bg-white py-5 pb-6 rounded-lg  hover:shadow-sm">
+                        <span className="ml-6">Convert from pdf</span>
                         <ul className="flex flex-col gap-5 justify-center items-center mt-5">
                             <li className="w-48 py-2 hover:border-indigo-100 hover:bg-indigo-100 hover:rounded-sm hover:shadow-md">
-                                <a href="#" className="flex h-9 items-center  justify-around" >
-                                    <div className="flex justify-center items-center gap-5">
-                                    <div className="h-auto ">
-                                        <img src="img/pdf to jpeg.png" alt="" className="max-w-full h-8 mr-1"/>
-                                    </div>
-                                    <span >PDF to JPG</span>
+                                <a href="#" className="flex h-9 items-center" >
+                                    <div className="flex justify-center items-center">
+                                    <img src="img/pdf to jpeg.png" alt="" className="max-w-full h-14"/>
+                                    <span>PDF to JPEG</span>
                                     </div>
                                 </a>
                             </li>
                             <li className=" w-48 py-2 hover:border-indigo-100 hover:bg-indigo-100 hover:rounded-sm hover:shadow-md">
-                                <a href="#" className="flex h-9 items-center  justify-around">
-                                <div className="flex justify-center items-center gap-5">
-                                    <div className="h-auto ">
-
-                                   <img src="img/pdf to jpeg.png" alt="" className="max-w-full h-8 mr-1"/>
-                                    </div>
-                                    <span className="">PDF to PNG</span>
+                                <a href="#" className="flex h-9 items-center">
+                                <div className="flex justify-center items-center">
+                                   <img src="img/pdf to jpeg.png" alt="" className="max-w-full h-14"/>
+                                    <span>PDF to PNG</span>
                                 </div>
                                 </a>
                             </li>
-                            <li className="w-48 py-2 hover:border-blue-200 hover:bg-blue-200 hover:rounded-sm hover:shadow-md">
-                                <a href="#" className="flex h-9 items-center gap-5 justify-around">
-                                <div className="flex justify-center items-center gap-5">
-                                    <div className="h-auto ">
-
-                                   <img src="img/pdf to word.png" alt="" className="max-w-full h-8 ml-2"/>
-                                    </div>
-                                    <span className="">PDF to Word</span>
+                            <li className="w-48 py-2 hover:border-purple-100 hover:bg-purple-100 hover:rounded-sm hover:shadow-md">
+                            <a href="#" className="flex h-9 items-center">
+                                <div className="flex justify-center items-center">
+                                   <img src="img/pdf to html.png" alt="" className="max-w-full h-14"/>
+                                    <span>PDF to HTML</span>
                                 </div>
                                 </a>
                             </li>
-                            <li className=" w-48 py-2 hover:border-green-100 hover:bg-green-100 hover:rounded-sm hover:shadow-md">
-                            <a href="#" className="flex h-9 items-center  justify-around">
-                                <div className="flex justify-center items-center gap-5">
-                                    <div className="h-auto ">
-
-                                   <img src="img/pdf to excel.png" alt="" className="max-w-full h-9 ml-2"/>
-                                    </div>
-                                    <span className="">PDF to Excel</span>
+                            <li className="w-48 py-2 hover:border-indigo-100 hover:bg-indigo-100 hover:rounded-sm hover:shadow-md">
+                                <a href="#" className="flex h-9 items-center">
+                                <div className="flex justify-center items-center">
+                                   <img src="img/pdf to word.png" alt="" className="max-w-full h-14"/>
+                                    <span>PDF to Word</span>
                                 </div>
                                 </a>
                             </li>
-                            <li className="w-48 py-2 hover:border-orange-100 hover:bg-orange-100 hover:rounded-sm hover:shadow-md">
-                            <a href="#" className="flex h-9 items-center justify-around">
-                                <div className="flex justify-center items-center gap-5">
-                                    <div className="h-auto ">
-
-                                   <img src="img/pdf to ppt.png" alt="" className="max-w-full h-9 mr-1"/>
-                                    </div>
-                                    <span className="">PDF to ppt</span>
+                            <li className=" w-48 py-2 hover:border-indigo-100 hover:bg-indigo-100 hover:rounded-sm hover:shadow-md">
+                            <a href="#" className="flex h-9 items-center">
+                                <div className="flex justify-center items-center">
+                                   <img src="img/pdf to excel.png" alt="" className="max-w-full h-14"/>
+                                    <span>PDF to Excel</span>
                                 </div>
                                 </a>
                             </li>
-                            <li className=" w-48 py-2 hover:border-violet-100 hover:bg-violet-100 hover:rounded-sm hover:shadow-md">
-                            <a href="#" className="flex h-9 items-center  justify-around">
-                                <div className="flex justify-center items-center gap-5">
-                                    <div className="h-auto  ">
-
-                                   <img src="img/pdf to html.png" alt="" className="max-w-full h-8 ml-2"/>
-                                    </div>
-                                    <span className="">PDF to HTML</span>
+                            <li className="w-48 py-2 hover:border-purple-100 hover:bg-purple-100 hover:rounded-sm hover:shadow-md">
+                            <a href="#" className="flex h-9 items-center">
+                                <div className="flex justify-center items-center">
+                                   <img src="img/pdf to ppt.png" alt="" className="max-w-full h-14"/>
+                                    <span>PDF to PPT</span>
                                 </div>
                                 </a>
-
                             </li>
+
                         </ul>
 
                     </div>
+    
                     <div>
                         <div className="border border-white w-60  bg-white py-5 pb-6 rounded-lg  hover:shadow-sm">
                         <span className="ml-6">Organize</span>
@@ -162,7 +146,7 @@ export function ProductDt({About,enter,setAbout,setEnter,isMobile}){
                             <li className="w-48 py-2 hover:border-zinc-100 hover:bg-zinc-100 hover:rounded-sm hover:shadow-md">
                                 <a href="#" className="flex h-9 items-center">
                                 <div className="flex justify-center items-center">
-                                   <img src="img/edit/page number copy.png" alt="" className="max-w-full h-14"/>
+                                   <img src="img/edit/page number.png" alt="" className="max-w-full h-14"/>
                                     <span>Page Number</span>
                                 </div>
                                 </a>
