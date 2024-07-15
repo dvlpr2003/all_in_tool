3df92dbe619fd612d26b36eab455ddae50590893
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-15 18:34:00 +0530

    sub menu

--- src/Header/MenuIcon.jsx
+++ src/Header/MenuIcon.jsx
@@ -4,7 +4,7 @@ import { RxCross2 } from "react-icons/rx";
 function IconMenu({setMenuIcon,MenuIcon}){
     return(
         <div className="flex justify-center items-center gap-5 menu" onClick={()=>setMenuIcon((e)=>!e)}>
-        <span className="min-[55px]:text-lg min-[660px]:text-2xl">Menu</span>
+        <span className="min-[55px]:text-lg min-[660px]:text-2xl">{MenuIcon?"Close":"Menu"}</span>
         {MenuIcon?<RxCross2  className="menu-icon min-[55px]:text-xl min-[660px]:text-2xl"/>:<HiOutlineMenuAlt1 className="menu-icon min-[55px]:text-xl min-[660px]:text-2xl" />}
         
 
