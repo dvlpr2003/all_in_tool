edfcf91d117863b4453a57a43ca2b928f4c304d7
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-15 18:22:21 +0530

    sub menu

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -5,7 +5,7 @@ import { ProductDt } from "./Submenu";
 import SideMenu from "./side menu/SideMenu";
 export default function Header(){
     const [enter,setEnter]=useState(false)
-    const [About,setAbout]=useState(false)
+    const [Tool,setTool]=useState(false)
     const [MenuIcon,setMenuIcon]=useState(false)
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
 // screen innerWidth listener useEffect function
@@ -27,19 +27,17 @@ export default function Header(){
         <>
         <div className="flex w-full justify-center items-center gap-4 max-w-screen-xl relative  ">
 
-
-        <div className=" justify-start h-auto">
+        <div className=" justify-start h-auto cursor-pointer">
           {/* logo */}
             <img src="img/logo-image/company.png" alt="logo" className="max-w-full h-9 min-[55px]:h-7 min-[660px]:h-7"/>
-
         </div>
 
         <div className="flex flex-wrap  justify-center items-center ml-3 cursor-pointer sm:hidden lg:flex min-[55px]:hidden ">
             
-            <div className={`text-slate-950 hover:text-indigo-600 ${enter?'text-zinc-500':""} px-5 py-3  flex items-center justify-center  `} onMouseLeave={!isMobile?()=>{setEnter(false)}:null} onMouseEnter={!isMobile?()=>{setEnter(true)}:null} onClick={isMobile?()=>{setEnter((e)=>!e); setAbout((e)=>!e)}:()=>{setEnter((e)=>!e); setAbout((e)=>!e)}} >
+            <div className={`text-slate-950 hover:text-indigo-600 ${enter?'text-zinc-500':""} px-5 py-3  flex items-center justify-center  `} onMouseLeave={!isMobile?()=>{setEnter(false)}:null} onMouseEnter={!isMobile?()=>{setEnter(true)}:null} onClick={isMobile?()=>{setEnter((e)=>!e); setTool((e)=>!e)}:()=>{setEnter((e)=>!e); setTool((e)=>!e)}} >
                 <div className="flex" >
                 <span >Tools</span>
-                <MdKeyboardArrowDown className="c-menu-arrow"/> 
+                <MdKeyboardArrowDown className={`c-menu-arrow ${Tool?"rotate-180":"rotate-0"} transition-all`}/> 
                 </div>
             </div>
 
@@ -61,9 +59,10 @@ export default function Header(){
         </div>
 
 
-        <ProductDt About={About} setAbout={setAbout} enter={enter} setEnter={setEnter} isMobile={isMobile}/>
+        <ProductDt Tool={Tool} setTool={setTool} enter={enter} setEnter={setEnter} isMobile={isMobile}/>
 
         {MenuIcon&&<SideMenu/>}
+
         </div>
         
         </>
@@ -76,7 +75,7 @@ function Buttons(){
   return(
     <div className="flex gap-5  min-[55px]:hidden lg:flex">
     <button className=" px-5  rounded-lg hover:bg-slate-100 border-0">Log in</button>
-    <button className="px-6 py-2 text-lg text-white rounded-lg border border-indigo-600 bg-indigo-600 font-bold  hover:bg-indigo-500 hover:border-indigo-500 hover:text-white" >Sign up free</button>
+    <button className="px-6 py-2 text-lg text-white rounded-lg border border-indigo-600 bg-indigo-600 font-bold  hover:bg-indigo-500 hover:border-indigo-500 hover:text-white transition-all" >Sign up free</button>
     </div>
     
   )
--- src/Header/Submenu.jsx
+++ src/Header/Submenu.jsx
@@ -5,10 +5,10 @@ import Organize from "./submenu items/Organize";
 import Edit from "./submenu items/Edit";
 
 
-export function ProductDt({About,enter,setAbout,setEnter,isMobile}){
+export function ProductDt({Tool,enter,setTool,setEnter,isMobile}){
    
     return(
-        <div className={` absolute border about-menu ${About?"flex":"hidden"} ${isMobile?"hidden":""} gap-5 text-black  shadow-md`}>
+        <div className={` absolute border about-menu ${Tool?"flex":"hidden"} ${isMobile?"hidden":""} gap-5 text-black  shadow-md  transition `}>
             <div className=" w-full mt-9 flex justify-center pb-7">
                 <div className="flex justify-evenly  flex-wrap  w-full">
                     <ConvertFromPdf/>
@@ -19,7 +19,7 @@ export function ProductDt({About,enter,setAbout,setEnter,isMobile}){
 
             </div>
  
-            <RxCross2 className="absolute right-3 top-2 font-bold text-2xl cursor-pointer hover:text-indigo-600" onClick={()=>setAbout(false)}/>
+            <RxCross2 className="absolute right-3 top-2 font-bold text-2xl cursor-pointer hover:text-indigo-600" onClick={()=>setTool(false)}/>
         </div>
     )
 }
