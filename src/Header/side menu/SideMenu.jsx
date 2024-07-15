0cb7362aaaf257090f32a350c73585c781a41c6e
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-15 18:30:40 +0530

    sub menu

--- src/Header/side menu/SideMenu.jsx
+++ src/Header/side menu/SideMenu.jsx
@@ -23,7 +23,7 @@ export default function SideMenu(){
                             <span className="font-bold">CONVERT FROM PDF</span>
                         </div>
                         <div className="ml-auto justify-end pr-3">
-                        <IoIosArrowDropdownCircle  className="text-xl cursor-pointer" />
+                        <IoIosArrowDropdownCircle  className={`text-xl cursor-pointer ${fromPdf?"rotate-180":"rotate-0"} transition-all`} />
                         </div>
                         </div>
                         <div className={`w-full h-auto ${fromPdf?"block":"hidden"} pb-3`}>
@@ -36,7 +36,7 @@ export default function SideMenu(){
                             <span className="font-bold">CONVERT TO PDF</span>
                         </div>
                         <div className="ml-auto justify-end pr-3">
-                        <IoIosArrowDropdownCircle className="text-xl cursor-pointer" />
+                        <IoIosArrowDropdownCircle className={`text-xl cursor-pointer ${toPdf?"rotate-180":"rotate-0"} transition-all`} />
                         </div>
                         </div>
                         <div className={`w-full h-auto ${toPdf?"block":"hidden"} pb-3`}>
@@ -50,7 +50,7 @@ export default function SideMenu(){
                                 <span className="font-bold">ORGANIZE</span>
                             </div>
                             <div className="ml-auto justify-end pr-3">
-                            <IoIosArrowDropdownCircle className="text-xl cursor-pointer" />
+                            <IoIosArrowDropdownCircle className={`text-xl cursor-pointer ${Organize?"rotate-180":"rotate-0"} transition-all`} />
                             </div>
                         </div>
                         <div className={`w-full h-auto ${Organize?"block":"hidden"} pb-3`}>
@@ -63,7 +63,7 @@ export default function SideMenu(){
                                     <span className="font-bold">EDIT</span>                            
                                 </div>
                                 <div className="ml-auto justify-end pr-3">
-                                <IoIosArrowDropdownCircle className="text-xl cursor-pointer" />
+                                <IoIosArrowDropdownCircle className={`text-xl cursor-pointer ${Edit?"rotate-180":"rotate-0"} transition-all`} />
                                 </div>
                         </div>
                         <div className={`w-full h-auto ${Edit?"block":"hidden"} pb-3`}>
