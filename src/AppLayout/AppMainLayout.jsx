74ea6aa7b9910d260c96d2076081ee5d16590462
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-15 15:49:55 +0530

    sub menu

--- src/AppLayout/AppMainLayout.jsx
+++ src/AppLayout/AppMainLayout.jsx
@@ -4,7 +4,7 @@ import Header from "../Header/Header";
 export default function AppLayout(){
     return(
         <>
-        <header className="border-solid flex gap-5 w-full justify-center py-7 min-[55px]:px-6 ">
+        <header className="border-solid flex gap-5 w-full justify-center py-7 min-[55px]:px-3 min-[660px]:px-6 ">
            <Header/>
            
         </header>
