20d1b88d518507cba570955968be57b6ab7d86bc
dvlpr2003 <gayathrigaya698@gmail.com>
2024-07-20 18:57:17 +0530

    side menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import AppLayout from "./AppLayout/AppMainLayout";
 import Home from "./UI/Home";
+import Jpgtopdf from "./convert to pdf/jpeg to pdf/jpg-to-pdf";
 
 function App(){
   const router = createBrowserRouter(
@@ -11,7 +12,13 @@ function App(){
           {
             element:(<Home/>),
             path:"/"
+          },
+          {
+            element:(<Jpgtopdf/>),
+            path:"jpg-to-pdf"
           }
+
+
         ]
 
       }
---
+++ src/convert to pdf/jpeg to pdf/jpg-to-pdf.jsx
@@ -0,0 +1,10 @@
+export default function Jpgtopdf(){
+    return(
+        <>
+        <section className="border border-red-400 min-h-screen">
+
+        </section>
+        </>
+
+    )
+}
