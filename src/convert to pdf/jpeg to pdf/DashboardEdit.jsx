fced6b941b375ce30dd4385e46365b9fb98cd26a
dvlpr2003 <gayathrigaya698@gmail.com>
2024-08-01 23:10:22 +0530

    mob res

--- src/convert to pdf/jpeg to pdf/DashboardEdit.jsx
+++ src/convert to pdf/jpeg to pdf/DashboardEdit.jsx
@@ -19,8 +19,7 @@ export default function DashboardEdit() {
     { id: '4', content: 'Item 4' },
     { id: '5', content: 'Item 5' },{ id: '6', content: 'Item 6' },{ id: '7', content: 'Item 7' },
     { id: '8', content: 'Item 8' },{ id: '9', content: 'Item 9' },{ id: '10', content: 'Item 10' },
-    { id: '8', content: 'Item 8' },{ id: '9', content: 'Item 9' },{ id: '10', content: 'Item 10' },
-
+  
  
   ]);
 
