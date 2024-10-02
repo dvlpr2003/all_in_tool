00ce69cd5d085ea6d00a1924dfe3a34349e9c60f
dvlpr2003 <gayathrigaya698@gmail.com>
2024-10-02 08:45:11 +0530

    annotate

--- src/convert to pdf/annotate_pdf/Nav/NavRight.jsx
+++ src/convert to pdf/annotate_pdf/Nav/NavRight.jsx
@@ -4,7 +4,30 @@ export function NavRight({handleConvert}){
     return(
     
 
-            <aside className={`min-[55px]:hidden lg:flex flex-col w-60 h-screen  py-8 overflow-y-auto bg-white border-l rtl:border-r rtl:border-l `}>
+        <aside className={`min-[55px]:hidden lg:flex flex-col w-60 h-screen   overflow-y-auto bg-white border-l rtl:border-r rtl:border-l  `}>
+            <div className=" w-full h-full mt-10 flex flex-col  items-center gap-4 pt-10 overflow-y-scroll pb-3">
+                <div className=" border w-auto h-auto shadow-xl">
+                    <img src="https://d3jq6id3uwlfp0.cloudfront.net/media/upload_Files/annotate_file/b38dc85f-7470-451c-8efe-92cec8015c4f_page_1.jpg" alt=""  className="w-auto h-56"/>
+                </div>
+                <div className=" border w-auto h-auto shadow-xl">
+                    <img src="https://d3jq6id3uwlfp0.cloudfront.net/media/upload_Files/annotate_file/b38dc85f-7470-451c-8efe-92cec8015c4f_page_1.jpg" alt=""  className="w-auto h-56"/>
+                </div>
+                <div className=" border w-auto h-auto shadow-xl">
+                    <img src="https://d3jq6id3uwlfp0.cloudfront.net/media/upload_Files/annotate_file/b38dc85f-7470-451c-8efe-92cec8015c4f_page_1.jpg" alt=""  className="w-auto h-56"/>
+                </div>
+                <div className=" border w-auto h-auto shadow-xl">
+                    <img src="https://d3jq6id3uwlfp0.cloudfront.net/media/upload_Files/annotate_file/b38dc85f-7470-451c-8efe-92cec8015c4f_page_1.jpg" alt=""  className="w-auto h-56"/>
+                </div>
+                <div className=" border w-auto h-auto shadow-xl">
+                    <img src="https://d3jq6id3uwlfp0.cloudfront.net/media/upload_Files/annotate_file/b38dc85f-7470-451c-8efe-92cec8015c4f_page_1.jpg" alt=""  className="w-auto h-56"/>
+                </div>
+
+
+
+
+
+
+            </div>
 
 
 
