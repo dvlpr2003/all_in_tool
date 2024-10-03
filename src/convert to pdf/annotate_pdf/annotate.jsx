3ebc6f6eb3222d96112ae1f738760fdb9c31bfbd
dvlpr2003 <gayathrigaya698@gmail.com>
2024-10-03 08:12:59 +0530

    annotate

--- src/convert to pdf/annotate_pdf/annotate.jsx
+++ src/convert to pdf/annotate_pdf/annotate.jsx
@@ -51,7 +51,7 @@ export default function AnnotatePdf(){
                     <DashboardNav/>
 
                 </div>
-                <div className="w-full border bg-slate-50">
+                <div className="w-full  bg-slate-50">
                     
                 </div>
 
