c65778941bfadd95176eef2cf099153156c3ef51
dvlpr2003 <gayathrigaya698@gmail.com>
2024-10-24 19:49:21 +0530

    annotate-non

--- src/convert to pdf/annotate_pdf/annotate.jsx
+++ src/convert to pdf/annotate_pdf/annotate.jsx
@@ -162,43 +162,43 @@ export default function AnnotatePdf(){
                 onContextMenu={handleContextMenu}                                           
                 ref={containerRef}
             >
-                <div className=" bg-inherit absolute top-[80px]  z-50 h-[60px] w-full flex justify-center items-center  ">
-                    <div className="w-full h-full relative flex justify-center items-center">
-                        <div className=" shadow-xl w-1/2 h-full absolute bg-white flex items-center">
+                <div className=" bg-inherit absolute top-[80px]  z-50 h-[35px] w-full flex justify-center items-center  ">
+                    <div className="w-full  h-full border border-black relative flex justify-center items-center">
+                        <div className=" shadow-xl w-full h-full absolute bg-white flex items-center">
 
                             <div className="flex-1 flex h-full justify-center items-center ">
                                 <div className="flex-1 h-full flex justify-center items-center border-r cursor-pointer">
-                                    <div className=" flex-1 flex justify-center items-center"><HiPencil  className="text-slate-700 text-xl"/></div>
+                                    <div className=" flex-1 flex justify-center items-center"><HiPencil  className="text-slate-700 min-[55px]:text-sm lg:text-xl"/></div>
                                 </div>
                                 <div className="flex-1 h-full flex justify-center items-center border-r cursor-pointer">
-                                    <div className="flex-1 flex justify-center items-center"> <PiTextTFill  className="text-slate-700 text-xl"/></div>
+                                    <div className="flex-1 flex justify-center items-center"> <PiTextTFill  className="text-slate-700 min-[55px]:text-sm lg:text-xl"/></div>
                                 </div>
                                 <div className="flex-1 h-full flex justify-center items-center border-r cursor-pointer">
-                                    <div className="flex-1 flex justify-center items-center"><RiShapesFill  className="text-slate-700 text-xl"/></div>
+                                    <div className="flex-1 flex justify-center items-center"><RiShapesFill  className="text-slate-700 min-[55px]:text-sm lg:text-xl"/></div>
                                 </div>
                                 <div className="flex-1 h-full flex justify-center items-center border-r cursor-pointer">
-                                    <div className="flex-1 flex justify-center items-center"><FaRegImage className="text-slate-700"/></div>
+                                    <div className="flex-1 flex justify-center items-center"><FaRegImage className="text-slate-700 min-[55px]:text-sm lg:text-xl"/></div>
                                 </div>
                                 <div className="flex-1 h-full flex justify-center items-center border-r cursor-pointer">
-                                    <div className="flex-1 flex justify-center items-center"><MdPanTool  className="text-slate-700 text-xl"/></div>
+                                    <div className="flex-1 flex justify-center items-center"><MdPanTool  className="text-slate-700 min-[55px]:text-sm lg:text-xl"/></div>
                                 </div>
 
                             </div>
                             <div className="flex-1 flex h-full justify-center items-center ">
                                 <div className="flex-1 h-full flex justify-center items-center cursor-pointer border-r">
-                                    <div className="flex-1 flex justify-center items-center"><BsArrowClockwise  className="text-slate-700 text-xl"/></div>
+                                    <div className="flex-1 flex justify-center items-center"><BsArrowClockwise  className="text-slate-700 min-[55px]:text-sm lg:text-xl"/></div>
 
                                 </div>
                                 <div className="flex-1 h-full flex justify-center items-center cursor-pointer border-r">
-                                    <div className="flex-1 flex justify-center items-center"><BsArrowCounterclockwise className="text-slate-700 text-xl"/></div>
+                                    <div className="flex-1 flex justify-center items-center"><BsArrowCounterclockwise className="text-slate-700 min-[55px]:text-sm lg:text-xl"/></div>
 
                                 </div>
                                 <div className="flex-1 h-full flex justify-center items-center cursor-pointer border-r">
-                                    <div className="flex-1 flex justify-center items-center"><MdDelete  className="text-slate-700 text-xl"/></div>
+                                    <div className="flex-1 flex justify-center items-center"><MdDelete  className="text-slate-700 min-[55px]:text-sm lg:text-xl"/></div>
 
                                 </div>
                                 <div className="flex-1 h-full flex justify-center items-center cursor-pointer ">
-                                    <div className="flex-1 flex justify-center items-center"><FaSave className="text-slate-700 text-xl" /></div>
+                                    <div className="flex-1 flex justify-center items-center"><FaSave className="text-slate-700 min-[55px]:text-sm lg:text-xl" /></div>
 
                                 </div>
 
